2f10ad4bb983818a4288fb3320b3c5e137091fe4
Maxim Litvyakov <litvy-maksim@yandex.ru>
2022-10-30 23:14:01 +0700

    Add faker and fixture

---
+++ tests/Resource/Fixture/UserFixture.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Resource\Fixture;
+
+use App\Entity\User;
+use App\Tests\Tools\FakerTools;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixture extends Fixture
+{
+    use FakerTools;
+
+    const REFERENCE = 'user';
+
+    public function load(ObjectManager $manager)
+    {
+        $email = $this->getFaker()->email;
+        $password = $this->getFaker()->password;
+        $user = (new User())->setEmail($email)->setPassword($password)->setRoles([User::ROLE_USER]);
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $this->addReference(self::REFERENCE, $user);
+    }
+}
